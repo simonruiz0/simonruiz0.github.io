18f54543bd2f18eb35154ecb2b0268113f794291
Eskil <eskildam@outlook.com>
2026-08-15 23:05:33 +0200

    Fixed astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://astronaut.github.io',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
 });
